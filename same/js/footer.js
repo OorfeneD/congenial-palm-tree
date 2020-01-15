a53a62e576e089c24e7b2d1012f4252924c983e3
Glitch (hello-express) <none>
2020-01-15 05:19:10 +0000

    🐯📇 Checkpoint ./same/js/footer.js:4023550/692 ./same/scss/style.scss:4023550/12290 ./same/scss/colors.scss:4023550/59

--- same/js/footer.js
+++ same/js/footer.js
@@ -4,31 +4,32 @@ $(document).ready(() => {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
   if(!filter(["/away"], location.pathname)){ 
-    for(let page = 0; page < Object.keys(pages).length; page++){
-      $(".leftFilter").append(`
-        <input type="radio" name="page" id="${Object.keys(pages)[page]}Page" class="statsPage">
-        <label for="${Object.keys(pages)[page]}Page" name="${Object.values(pages)[page]}" onclick="$(location).attr('href', 'http://www.iwant.glitch.me/${page == 0 ? "" : Object.keys(pages)[page]}')"></label>
+    for(let page = 0; page < Object.keys(langSet[lang]["pages"]).length; page++){
+      console.log(page)
+      $(".rightFilter").append(`
+        <input type="radio" name="page" id="${Object.keys(langSet[lang]["pages"])[page]}Page" class="statsPage">
+        <label for="${Object.keys(langSet[lang]["pages"])[page]}Page" name="${Object.values(langSet[lang]["pages"])[page]}"></label>
       `)
     }
-    $(".leftFilter").css({left: "calc(50vw - 457px)"}).append(`    
-      <div class="graphMemeBox">
-        <input type="range" name="graphMeme" class="graphMeme" min="0" max="${memes.length-1}" step="1" value="0" oldvalue="0" orient="vertical" onmousemove="graphMemeBigMouseMove(this)">
-      </div> 
-    `)    
+    // $(".leftFilter").css({left: "calc(50vw - 457px)"}).append(`    
+    //   <div class="graphMemeBox">
+    //     <input type="range" name="graphMeme" class="graphMeme" min="0" max="${memes.length-1}" step="1" value="0" oldvalue="0" orient="vertical" onmousemove="graphMemeBigMouseMove(this)">
+    //   </div> 
+    // `)    
 /////-----------------------------------------------------------------------------------------------------------------------------------------------------------/////
 /////-----------------------------------------------------------------------------------------------------------------------------------------------------------/////
-    if($(".rightFilter")){
-      $(".rightFilter").css({right: "calc(50vw - 457px)"})
-      for(let i = 0; i < streamers.length; i++){
-        $(".rightFilter").append(`
-          <div n="${i}" style="order: ${i};${cookie["loadGraph"].slice(i, +i+1) == 0 && location.pathname == "/"? "display: none" : ""}">
-            <input type="radio" name="rightFilter" id="${streamers[i].toLowerCase()}"><label n="${i}" for="${streamers[i].toLowerCase()}" full="${streamers[i]}"></label>
-            <a target="_blank" filter="${streamers[i]}" ${location.pathname != "/setting" ? `href="${location.pathname.slice(1)}?c=${streamers[i]}` : ""}></a>
-          </div>
-        `)   
-      }
-      if(window.location.search){$(`.rightFilter #${streamers[get["c"]].toLowerCase()}`).prop("checked", true)}
-    }    
+    // if($(".rightFilter")){
+    //   $(".rightFilter").css({right: "calc(50vw - 457px)"})
+    //   for(let i = 0; i < streamers.length; i++){
+    //     $(".rightFilter").append(`
+    //       <div n="${i}" style="order: ${i};${cookie["loadGraph"].slice(i, +i+1) == 0 && location.pathname == "/"? "display: none" : ""}">
+    //         <input type="radio" name="rightFilter" id="${streamers[i].toLowerCase()}"><label n="${i}" for="${streamers[i].toLowerCase()}" full="${streamers[i]}"></label>
+    //         <a target="_blank" filter="${streamers[i]}" ${location.pathname != "/setting" ? `href="${location.pathname.slice(1)}?c=${streamers[i]}` : ""}></a>
+    //       </div>
+    //     `)   
+    //   }
+    //   if(window.location.search){$(`.rightFilter #${streamers[get["c"]].toLowerCase()}`).prop("checked", true)}
+    // }    
   }
   
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
