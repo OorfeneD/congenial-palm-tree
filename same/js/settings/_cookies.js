444282a8eb068cb34f75704e31b823b850557170
Glitch (hello-express) <none>
2020-02-05 21:31:14 +0000

    📺🎳 Checkpoint ./same/js/settings/_cookies.js:4023550/833

--- same/js/settings/_cookies.js
+++ same/js/settings/_cookies.js
@@ -41,13 +41,15 @@ for(let p = 0; p < bfB.length; p++){
       let lostPages = pages.slice(0);
       for(let i = 0; i < pages.length; i++){
         let key = keys[i];
-        if(!key || !filter(pages, key))
-          // lostPages = uu == 0 
-          // ? lostPages.slice(1) 
-          //   : uu == pages.length-1 
-          //   ? lostPages.slice(0, -1) 
-          //     : (lostPages.slice(0, uu-1) + "," + lostPages.slice(uu)).split(",").filter(e => e);
-        console.log(key)
+        if(pages[i] == )
+          lostPages[i] = "*"
+          // lostPages = (i == 0 
+          //   ? "*," + lostPages.slice(1) 
+          //     : i == pages.length-1 
+          //     ? lostPages.slice(0, -1) + ",*"
+          //       : lostPages.slice(0, i) + ",*," + lostPages.slice(i+1)
+          // ).split(",").filter(e => e);
+        console.log(lostPages)
       }
     }
   }
