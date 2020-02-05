6bda9154a89428119944d1f47802491b735d861e
Glitch (hello-express) <none>
2020-02-05 12:57:16 +0000

    👨‍👨‍👦🚌 Checkpoint ./same/js/_cookie.js:4023550/1469

--- same/js/_cookie.js
+++ same/js/_cookie.js
@@ -1,25 +1,32 @@
 var cookie = {},
     cookieDOM = document.cookie.split("; "),
     cookieDate = "Fri, 7 Aug 2020 00:00:00 UTC";
-for(let i = 0; i < cookieDOM.length; i++){
-  cookie[cookieDOM[i].split("=")[0]] = cookieDOM[i].split("=")[1];
+for(let i = 0; i < cookieDOM.length; i++){ 
+  let key = cookieDOM[i].split("=")[0],
+      value = cookieDOM[i].split("=")[1];
+  cookie[key] = value.slice(0, 1) != "{" ? value : {}
+  if(value.slice(0, 1) == "{"){
+    let cookieArr = value.slice(1, -2).split(",")
+    for(let u = 0; u < cookieArr.length; u++){
+      cookie[key][cookieArr[u].split(":")[0]] = cookieArr[u].split(":")[1]
+    }
+  }
 }  
 
 if(!cookie["autoload"]){
-  let result = "{";
+  let result = {};
   for(let i = 0; i < Object.keys(langSet[cookie["lang"]]["pages"]).length; i++){
-    result += Object.keys(langSet[cookie["lang"]]["pages"])[i]+": false,"
+    result[Object.keys(langSet[cookie["lang"]]["pages"])[i]] = "false"
   }
-  result += "}";
   cookie["autoload"] = result;
   document.cookie = `autoload=${cookie["autoload"]};expires=${cookieDate}`;
 }
 
 if(!cookie["lang"] || !langSet[cookie["lang"]]){
   cookie["lang"] = Object.keys(langSet)[0];
-  document.cookie = `lang=${Object.keys(langSet)[0]};expires=${cookieDate}`;
+  document.cookie = `lang=${cookie["lang"]};expires=${cookieDate}`;
 }
 if(!cookie["theme"] || !colorSet[cookie["theme"]]){
   cookie["theme"] = Object.keys(colorSet)[0];
-  document.cookie = `theme=${Object.keys(colorSet)[0]};expires=${cookieDate}`;
+  document.cookie = `theme=${cookie["theme"]};expires=${cookieDate}`;
 }
