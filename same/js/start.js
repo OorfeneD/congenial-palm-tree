5fa10a6081a9371cec04748adf149e39de2b04a7
Glitch (hello-express) <none>
2020-02-07 14:59:37 +0000

    🚖🔋 Checkpoint ./same/js/start.js:4023550/3075 ./index/index.html:4023550/2028

--- same/js/start.js
+++ same/js/start.js
@@ -1,14 +1,25 @@
-$.getScript("index/alert.js");
+let scriptsArr = {
+  same: {
+    func: ["return"],
+    objects: ["colors", "cookies", "languages", "", "", ""],
+    default: ["load"],
+  },
+  index: ["client"],
+}
 
-
-    <script src="_/settings/pages.js"></script>
-    <script src="_/settings/languages.js"></script>
-    <script src="_/settings/colors.js"></script>
-    <script src="_/_/func.js"></script>
-    <script src="_/_/data.js"></script>
-    <script src="_/settings/cookies.js"></script>
-    <script src="_/_/funcGet.js"></script>
-    <script src="_/_/funcSet.js"></script>
-    <script src="_/_/load.js"></script>
-    <script src="_/settings/smiles.js"></script>
-    <script src="index/client.js"></script>
+$("head").append(`
+<script src="_/_/func.js"></script>
+  <script src="_/settings/pages.js"></script>
+  <script src="_/settings/languages.js"></script>
+  <script src="_/settings/colors.js"></script>
+<script src="_/settings/cookies.js"></script>
+<script src="_/settings/smiles.js"></script>
+  
+  <script src="_/_/data.js"></script>
+  
+  <script src="_/_/funcGet.js"></script>
+  <script src="_/_/funcSet.js"></script>
+  <script src="_/_/load.js"></script>
+  
+  <script src="index/client.js"></script>
+`)
