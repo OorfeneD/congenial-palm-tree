f809ef150af9b7dbbb791271122b4faf3ac2b025
Glitch (hello-express) <none>
2020-02-07 17:52:57 +0000

    👹🏀 Checkpoint ./scss/colors.scss:4023550/7057 ./scss/mixin.scss:4023550/390 ./scss/style.scss:4023550/29774 ./js/func/doit.js:4023550/10171 ./js/func/return.js:4023550/5836 ./js/func/settings.js:4023550/1586 ./js/objects/colors.js:4023550/756 ./js/objects/cookies.js:4023550/5350 ./js/objects/database.js:4023550/8216 ./js/objects/get.js:4023550/214 ./js/objects/hash.js:4023550/472 ./js/objects/icons.js:4023550/974 ./js/objects/language.js:4023550/886 ./js/objects/languages.js:4023550/2820 ./js/objects/pages.js:4023550/4323 ./html/index.html:4023550/2851 ./js/client.js:4023550/4734

---
+++ js/objects/languages.js
@@ -0,0 +1,93 @@
+const langSet = {
+  ru: {
+    loading: "Загружаем",
+    pages: {
+      main: "Графики",
+      fbi: "FBI",
+      notes: "Заметки",
+      tags: "Теги",
+      archive: "Архив",
+      calendar: "Календарь",
+      settings: "Настройки",
+      database: "База данных",
+    },
+    menu: {
+      getTheme: "Тема сайта",
+      autoload: "Автозагрузка",
+      autoloadcompleted: "Всё загружено",
+      filter: {
+        name: "Фильтр",
+        same: "Общее",
+        theme: "Оформление",
+      },
+    },
+    settings: {
+      activePage: "Активация при заходе на страницу",
+      hueRotate: "Настройка палитры сайта",
+    },
+    time: {
+      online: "онлайн",
+      today: "сегодня",
+      yesterday: "вчера",
+      month: {
+        m0: "января",
+        m1: "февраля",
+        m2: "марта",
+        m3: "апреля",
+        m4: "мая",
+        m5: "июня",
+        m6: "июля",
+        m7: "августа",
+        m8: "сентября",
+        m9: "октября",
+        m10: "ноября",
+        m11: "декабря",
+      }
+    },
+  },  
+  en: {
+    loading: "Loading",
+    pages: {
+      main: "Main",
+      fbi: "FBI",
+      notes: "Notes",
+      tags: "Tags",
+      archive: "Archive",
+      calendar: "Calendar",
+      settings: "Settings",
+      database: "Database",
+    },
+    menu: {
+      getTheme: "Site Theme",
+      autoload: "Autoload",
+      autoloadcompleted: "All uploaded",
+      filter: {
+        name: "Filter",
+        same: "Same",
+        theme: "Decor",
+      },
+    },
+    settings: {
+      activePage: "Activation when entering the page",
+    },
+    time: {
+      online: "online",
+      today: "today",
+      yesterday: "yesterday",
+      month: {
+        m0: "January",
+        m1: "February",
+        m2: "March",
+        m3: "April",
+        m4: "May",
+        m5: "June",
+        m6: "July",
+        m7: "August",
+        m8: "September",
+        m9: "October",
+        m10: "November",
+        m11: "December",
+      }
+    },
+  },    
+}; 
