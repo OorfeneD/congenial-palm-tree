54559062b400b73a50e4cc502b4e97014f07d1cf
Glitch (hello-express) <none>
2020-02-11 15:41:39 +0000

    🏋🚈 Checkpoint ./scss/mixin.scss:4023550/449 ./scss/style.scss:4023550/86 ./js/pages/settings.js:4023550/10

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -30,7 +30,7 @@ function loadSettings(type){
               `)}
             $("li[for='cookieRightFilter'] h8").append(`
               <input type="checkbox" id="${list[i]}Cookie" oninput="objectCookie(this);">
-              <label for="${list[i]}Cookie" icon="${list[i]}" bg="_h:dark_c:color_ch:green"></label><br>
+              <label for="${list[i]}Cookie" icon="${list[i]}" bg="_h:dark_c:color_ch:color"></label><br>
             `);
             $(`input#${list[i]}Cookie`).prop("checked", +cookie[`turn_${list[i]}`][check])
           }       
