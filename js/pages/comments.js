7663fda92b6447b0341c396d54f1fc07056e21ab
Glitch (ican) <none>
2020-02-23 14:20:56 +0000

    🎮🎉 Checkpoint ./js/pages/comments.js:4023550/119 ./js/func/doit.js:4023550/479 ./js/func/return.js:4023550/526

--- js/pages/comments.js
+++ js/pages/comments.js
@@ -72,7 +72,7 @@ function loadComments(type, result, step){
                     <a target="_blank" href="${url}">
                       <b>[${ts}] #${user}:</b> ${mes}
                     </a>
-                    <div delete ></div>
+                    <div delete onclick="dlt(this, 'message', ${mArr[i]["t"]});"></div>
                   </div>
                 `);
               }
