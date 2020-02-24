2942f375f3ac5f90ad5e692484b19d000e9edbd2
Glitch (ican) <none>
2020-02-24 16:25:42 +0000

    🚂🏪 Checkpoint ./js/objects/get.js:4023550/704 ./js/func/filter.js:4023550/14 ./scss/style.scss:4023550/44 ./js/client.js:4023550/3475

--- js/client.js
+++ js/client.js
@@ -24,42 +24,37 @@ function start(ths, pass = ""){
 function getContent(type, step = 0){
   createGet();
   let oldget = get;
-  if(type == "settings"){
-    loadSettings(type)
-  }else if(filter(["main"], type)){
-    // $.ajax({
-    //   url: "listStream",
-    //   data: {step: step, limit: loadLimit},
-    //   method: 'get',
-    //   success: data => {
-    //     console.log(data);
-    //     loadMain(type, data, step); 
-    //   }
-    // })
-  }else if(filter(["fbi", "notes", "tags"], type)){
-    if(!$("ul div[load]").length)
-      $("ul").append(`<div view="button" load></div>`)
-    $.ajax({
-      url: "listStream",
-      data: {
-        type: type, 
-        from: loadLimit*step, 
-        limit: loadLimit, 
-        channel: get[pathname]["channel"] || 0, 
-        sort: get[pathname]["sort"] || "DESC",
-        by: get[pathname]["by"] || "sI",
-        date: get[pathname]["date"] || 0,
-        duration: get[pathname]["duration"] || 0,
-        pop: get[pathname]["pop"] || 0,
-        sID: get[pathname]["sID"] || 0,
-      },
-      method: 'get',
-      error: err => setTimeout(() => {if(pathname == type) getContent(type, step)}, 3000),
-      success: data => {
-        console.log(data)
-        loadComments(type, data, step, oldget);
-        $(`.loadCode input`).prop("checked", false)
-      }
-    })
+  if(!$("ul div[load]").length && !filter(["settings"], pathname)){
+    $("ul").append(`<div view="button" load></div>`)
+    $("main").css({cursor: "wait"})
+  }
+  
+  switch(type){
+    case "settings": loadSettings(type); break;
+    case "main":  break;
+    case "fbi": case "notes": case "tags":  
+      $.ajax({
+        url: "listStream",
+        data: {
+          type: type, 
+          from: loadLimit*step, 
+          limit: loadLimit, 
+          channel: get[pathname]["channel"] || 0, 
+          sort: get[pathname]["sort"] || "DESC",
+          by: get[pathname]["by"] || "sI",
+          date: get[pathname]["date"] || 0,
+          duration: get[pathname]["duration"] || 0,
+          pop: get[pathname]["pop"] || 0,
+          sID: get[pathname]["sID"] || 0,
+        },
+        method: 'get',
+        error: err => setTimeout(() => {if(pathname == type) getContent(type, step)}, 3000),
+        success: data => {
+          $("main").css({cursor: ""})
+          loadComments(type, data, step, oldget);
+          $(`.loadCode input`).prop("checked", false)
+        }
+      })
+    break;
   }
 }
--- js/objects/get.js
+++ js/objects/get.js
@@ -1,37 +1,29 @@
 
 let get = {};
 function createGet(){
-  // let result = "";
   if(!get[pathname]) get[pathname] = {}
   if(window.location.search.slice(1) != ""){
     let href = window.location.search.slice(1).split("&");
     for(let i = 0; i < href.length; i++){
       get[pathname][href[i].split("=")[0]] = href[i].split("=")[1];
-      // result += `&${href[i].split("=")[0]}=${href[i].split("=")[1]}`
     }
-    // result = "?" + result.slice(1)
   }
-  // history.replaceState('', null, pathname + result);
 }
-// createGet()
 
-function newGet(string){
+function getToObj(string){
   if(!get[pathname]) get[pathname] = {}
-  if(window.location.search.slice(1) != ""){
-    string = string.slice(1).split("&");
-    for(let i = 0; i < string.length; i++){
-      get[pathname][string[i].split("=")[0]] = string[i].split("=")[1];
-    }
+  string = string.slice(1).split("&");
+  for(let i = 0; i < string.length; i++){
+    get[pathname][string[i].split("=")[0]] = string[i].split("=")[1];
   }
 }
 
 function getToString(){
-  if(get[pathname]){
-    let string = window.location.search.slice(1).split("&");
-    for(let i = 0; i < string.length; i++){
-      get[pathname][string[i].split("=")[0]] = string[i].split("=")[1];
-      // result += `&${string[i].split("=")[0]}=${string[i].split("=")[1]}`
+  if(get[pathname] && Object.keys(get[pathname]).length){
+    let result = "";
+    for(let i = 0; i < Object.keys(get[pathname]).length; i++){
+      result += `&${Object.keys(get[pathname])[i]}=${Object.values(get[pathname])[i]}`
     }
-    // result = "?" + result.slice(1)
-  }
+    return "?" + result.slice(1)
+  }else{return ""}
 }
