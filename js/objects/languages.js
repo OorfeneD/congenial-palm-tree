78f926b446386f90306582f8bb513089b76949af
Glitch (ican) <none>
2020-03-01 14:08:10 +0000

    💠📮 Checkpoint ./js/objects/languages.js:4023550/1233

--- js/objects/languages.js
+++ js/objects/languages.js
@@ -88,20 +88,6 @@ var langObj = {
       online: "онлайн",
       today: "сегодня",
       yesterday: "вчера",
-      month: {
-        m0: "января",
-        m1: "февраля",
-        m2: "марта",
-        m3: "апреля",
-        m4: "мая",
-        m5: "июня",
-        m6: "июля",
-        m7: "августа",
-        m8: "сентября",
-        m9: "октября",
-        m10: "ноября",
-        m11: "декабря",
-      }
     },
     help: {
       archive: "» Архив",
@@ -115,7 +101,7 @@ var langObj = {
         cookie_link: "Отображать ссылки",
         cookie_smile: "Отображать смайлики",
         cookie_maxline: "Линия максимального значения",
-        cookie_midnight: "Линия полночи",
+        cookie_midnight: "Линия полуночи",
       },
       fn: {
         main: "FBI<br>Заметки<br>Теги",
@@ -224,20 +210,6 @@ var langObj = {
       online: "online",
       today: "today",
       yesterday: "yesterday",
-      month: {
-        m0: "January",
-        m1: "February",
-        m2: "March",
-        m3: "April",
-        m4: "May",
-        m5: "June",
-        m6: "July",
-        m7: "August",
-        m8: "September",
-        m9: "October",
-        m10: "November",
-        m11: "December",
-      }
     },
     help: {
       archive: "» Archive",
