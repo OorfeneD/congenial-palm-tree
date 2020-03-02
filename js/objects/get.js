9b656791c0224d178ae96035b350d8820db5779a
Glitch (ican) <none>
2020-03-02 15:12:41 +0000

    💼🐵 Checkpoint ./js/objects/get.js:4023550/215

--- js/objects/get.js
+++ js/objects/get.js
@@ -7,6 +7,8 @@ function createGet(){
     for(let i = 0; i < href.length; i++){
       get[pathname][href[i].split("=")[0]] = href[i].split("=")[1];
     }
+    console.log(JSON.stringify(get).replace(/"/g,""))
+    document.cookie = `get=${JSON.stringify(get).replace(/"/g,"")};expires=${cookieDate}`;
   }
 }
 
