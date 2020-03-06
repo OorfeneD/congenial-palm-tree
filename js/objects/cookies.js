fda384d5591166681504ee8f6439e7f49b4cdc0b
Glitch (ican) <none>
2020-03-06 18:22:36 +0000

    ⭐️☕️ Checkpoint ./js/objects/cookies.js:4023550/2867

--- js/objects/cookies.js
+++ js/objects/cookies.js
@@ -5,13 +5,7 @@ var cookie = {},
 for(let i = 0; i < cookieDOM.length; i++){ 
   let key = cookieDOM[i].split("=")[0],
       value = cookieDOM[i].split("=")[1];
-  cookie[key] = value.slice(0, 1) != "{" ? value : {}
-  if(value.slice(0, 1) == "{"){
-    let cookieArr = value.slice(1, -1).split(",")
-    for(let u = 0; u < cookieArr.length; u++){
-      cookie[key][cookieArr[u].split(":")[0]] = cookieArr[u].split(":")[1]
-    }
-  }
+  cookie[key] = filter(["{"], value) ? JSON.parse(value) : value
 }  
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -24,7 +18,7 @@ if(!cookie["theme"] || !colorObj[cookie["theme"]]){
   cookie["theme"] = Object.keys(colorObj)[0];
   document.cookie = `theme=${cookie["theme"]};expires=${cookieDate}`;
 }
-if(!cookie["UTC"] || isNaN(cookie["UTC"]) || cookie["UTC"] > 56 || cookie["UTC"] < -44){
+if(!cookie["UTC"] || isNaN(cookie["UTC"]) || +cookie["UTC"] > 56 || +cookie["UTC"] < -44){
   cookie["UTC"] = new Date().getTimezoneOffset() / -15;
   document.cookie = `UTC=${cookie["UTC"]};expires=${cookieDate}`;
 }
@@ -32,41 +26,41 @@ if(!cookie["UTC"] || isNaN(cookie["UTC"]) || cookie["UTC"] > 56 || cookie["UTC"]
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-for(let p = 0; p < pageSet["bottomMenu"]["list"].length; p++){
-  let turn = `turn_${pageSet["bottomMenu"]["list"][p]}`;
-  if(!cookie[turn]){
-    cookie[turn] = {};
-    for(let i = 0; i < settingsPages.length; i++){
-      let key = settingsPages[i];
-      cookie[turn][key] = pageSet["bottomMenu"][turn] 
-        ? filterOnly(["turn_help"], turn) ? "1" 
-        : filterOnly(pageSet["bottomMenu"][turn], key) ? "1" : "0" : "0"
-    }
-  }else{
-    let keys = Object.keys(cookie[turn]),
-        values = Object.values(cookie[turn]);
-    for(let i = 0; i < keys.length; i++){
-      let key = keys[i],
-          value = values[i];
-      if(!filterOnly(settingsPages, key)){
-        delete cookie[turn][key];
-      }else{
-        cookie[turn][key] = !filterOnly(["0", "1"], value)
-          ? "0" : pageSet["bottomMenu"][turn] 
-            ? filterOnly(pageSet["bottomMenu"][turn], key)
-              ? "1" : value : value;        
-      }
-    }
-    if(Object.keys(cookie[turn]).length != settingsPages.length){      
-      for(let i = 0; i < settingsPages.length; i++){
-        if(!filterOnly(cookie[turn], settingsPages[i]))
-          cookie[turn][settingsPages[i]] = filterOnly(["turn_help"], turn) ? "1" 
-            : filterOnly(pageSet["bottomMenu"][turn], settingsPages[i]) ? "1" : "0"          
-      }    
-    }
-  }
-  document.cookie = `${turn}=${JSON.stringify(cookie[turn]).replace(/"/g,"")};expires=${cookieDate}`;  
-}
+// for(let p = 0; p < pageSet["bottomMenu"]["list"].length; p++){
+//   let turn = `turn_${pageSet["bottomMenu"]["list"][p]}`;
+//   if(!cookie[turn]){
+//     cookie[turn] = {};
+//     for(let i = 0; i < settingsPages.length; i++){
+//       let key = settingsPages[i];
+//       cookie[turn][key] = pageSet["bottomMenu"][turn] 
+//         ? filterOnly(["turn_help"], turn) ? "1" 
+//         : filterOnly(pageSet["bottomMenu"][turn], key) ? "1" : "0" : "0"
+//     }
+//   }else{
+//     let keys = Object.keys(cookie[turn]),
+//         values = Object.values(cookie[turn]);
+//     for(let i = 0; i < keys.length; i++){
+//       let key = keys[i],
+//           value = values[i];
+//       if(!filterOnly(settingsPages, key)){
+//         delete cookie[turn][key];
+//       }else{
+//         cookie[turn][key] = !filterOnly(["0", "1"], value)
+//           ? "0" : pageSet["bottomMenu"][turn] 
+//             ? filterOnly(pageSet["bottomMenu"][turn], key)
+//               ? "1" : value : value;        
+//       }
+//     }
+//     if(Object.keys(cookie[turn]).length != settingsPages.length){      
+//       for(let i = 0; i < settingsPages.length; i++){
+//         if(!filterOnly(cookie[turn], settingsPages[i]))
+//           cookie[turn][settingsPages[i]] = filterOnly(["turn_help"], turn) ? "1" 
+//             : filterOnly(pageSet["bottomMenu"][turn], settingsPages[i]) ? "1" : "0"          
+//       }    
+//     }
+//   }
+//   document.cookie = `${turn}=${JSON.stringify(cookie[turn])};expires=${cookieDate}`;  
+// }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -117,9 +111,9 @@ if(!cookie["turn"]){
     //     }    
     //   }
     // }
-    // document.cookie = `${turn}=${JSON.stringify(cookie[turn]).replace(/"/g,"")};expires=${cookieDate}`;  
   }
 }
+document.cookie = `turn=${JSON.stringify(cookie["turn"])};expires=${cookieDate}`;  
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -147,7 +141,7 @@ if(!cookie["hueRotate"]){
     }    
   }
 }
-document.cookie = `hueRotate=${JSON.stringify(cookie["hueRotate"]).replace(/"/g,"")};expires=${cookieDate}`;  
+document.cookie = `hueRotate=${JSON.stringify(cookie["hueRotate"])};expires=${cookieDate}`;  
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
