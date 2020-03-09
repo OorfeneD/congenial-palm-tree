d019971bd04d72b21812404192dfb0b99bfb2bb6
Glitch (ican) <none>
2020-03-09 13:57:10 +0000

    🍁👴 Checkpoint ./scss/style.scss:4023550/124 ./js/pages/archive.js:4023550/16 ./js/pages/best.js:4023550/73 ./js/pages/main.js:4023550/10

--- js/pages/main.js
+++ js/pages/main.js
@@ -69,8 +69,8 @@ function loadMain(type, result, step, oldget){
             let width = (gmax-gmin+xW(ch))*xW(ch) < widthLi() ? widthLi() : Math.round((gmax-gmin+xW(ch))/5)*5*xW(ch);
             let rangeMax = Math.round((gmax-gmin)/5) - widthLi()/(5*xW(ch));
                 rangeMax = rangeMax < 0 ? 0 : rangeMax + 1;
-            let bottomThumb = (widthLi(40)/(rangeMax+1))*1.5 > widthLi(60) ? widthLi(60) : (widthLi(60)/(rangeMax+1))*1.5,
-                rightThumb  = (heightLi()/Object.keys(memes).length)*1.5 > heightLi() ? heightLi() : (heightLi()/Object.keys(memes).length)*1.5
+            let bottomThumb = (widthLi(60)/(rangeMax+1))*1.5 > widthLi(60) ? widthLi(60) : (widthLi(60)/(rangeMax+1))*1.5,
+                rightThumb  = (heightLi(60)/Object.keys(memes).length)*1.5 > heightLi(60) ? heightLi(60) : (heightLi(60)/Object.keys(memes).length)*1.5
             
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
