8d75195a60114c48eb3256c840e4591bafb447a2
Glitch (ican) <none>
2020-04-03 22:16:25 +0000

    🦂👓 Checkpoint ./js/pages/comments.js:4023550/141 ./js/objects/smiles.js:4023550/52

--- js/pages/comments.js
+++ js/pages/comments.js
@@ -53,8 +53,7 @@ function loadComments(type, result, step, oldget){
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
               if(!$(`li[sID="${sID}"]`).length){
-                let order = !filter(["time"], dateType) ? -mArr[0].t % 172800000 : 0
-                // console.log( mArr[0].t % 172800000 )
+                let order = filter(["time"], dateType) ? 0 : -mArr[0].t % 172800000
                 $("main ul div[load]").before(`
                   <input type="checkbox" id="arrow_comments${sID}" ${turn("arrow") ? "checked" : ""}>
                   <label for="arrow_comments${sID}" icon="arrow" username="${ch}" style="order: ${order}"></label>
